cf821252aab187caea47a20ee64613d2b4e15c86
duckida <duckidaweb@gmail.com>
2026-07-24 20:11:14 +0200

    make it update more often

--- hardware/nfc_test.py
+++ hardware/nfc_test.py
@@ -8,10 +8,11 @@ print("")
 
 while True:
     reader.init()
+    
     (stat, tag_type) = reader.request(reader.REQIDL)
     if stat == reader.OK:
         (stat, uid) = reader.SelectTagSN()
         if stat == reader.OK:
             card = int.from_bytes(bytes(uid), "little", False)
             print("CARD ID: " + str(card))
-    utime.sleep_ms(500)
+    utime.sleep_ms(100)
